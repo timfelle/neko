a7081e3478469debe21125a292043c100a68e861
Tim Felle Olsen <tife@dtu.dk>
2025-06-24 12:41:44 +0200

    Introduce the new ksp table for scalars as well.

--- src/scalar/scalars.f90
+++ src/scalar/scalars.f90
@@ -36,6 +36,7 @@ module scalars
   use num_types, only: rp
   use scalar_pnpn, only: scalar_pnpn_t
   use scalar_scheme, only: scalar_scheme_t
+  use scalar_aux, only: scalar_step_info
   use mesh, only: mesh_t
   use space, only: space_t
   use gather_scatter, only: gs_t
@@ -43,12 +44,12 @@ module scalars
   use time_step_controller, only: time_step_controller_t
   use json_module, only: json_file
   use json_utils, only: json_get, json_get_or_default, json_extract_object, &
-                        json_extract_item
+       json_extract_item
   use field, only: field_t
   use field_series, only: field_series_t
   use field_registry, only: neko_field_registry
   use checkpoint, only: chkp_t
-  use krylov, only: ksp_t
+  use krylov, only: ksp_t, ksp_monitor_t
   use logger, only: neko_log, LOG_SIZE, NEKO_LOG_VERBOSE
   use user_intf, only: user_t
   use utils, only: neko_error
@@ -188,11 +189,15 @@ contains
     type(time_scheme_controller_t), intent(inout) :: ext_bdf
     type(time_step_controller_t), intent(inout) :: dt_controller
     integer :: i
+    type(ksp_monitor_t), dimension(size(this%scalar_fields)) :: ksp_results
 
     ! Iterate through all scalar fields
     do i = 1, size(this%scalar_fields)
-       call this%scalar_fields(i)%step(time, ext_bdf, dt_controller)
+       call this%scalar_fields(i)%step(time, ext_bdf, dt_controller, &
+            ksp_results(i))
     end do
+
+    call scalar_step_info(time, ksp_results)
   end subroutine scalars_step
 
   !> Restart from checkpoint data
